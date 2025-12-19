3398df47f9431318df9454fb93388f4d6429faac
noproto <noproto@zeroday.engineering>
2025-12-18 20:23:26 -0500

    Challenge 6

--- 1/6/6.py
+++ 1/6/6.py
@@ -16,7 +16,7 @@ def xor_two_equal_len_byte_arrays(first, second):
     return bytes([first[i] ^ second[i] for i in range(0, len(first))])
 
 def score_printable(attempt):
-    return sum([1 for x in attempt if x in bytes(string.ascii_letters, "ascii")])
+    return sum([1 for x in attempt if x in bytes(string.ascii_letters+" .", "ascii")])
 
 def repeating_key_xor(plaintext, xor_key):
     ciphertext = b""
@@ -32,20 +32,20 @@ min_keysize = 2
 max_keysize = 40
 ks_edit_distance = []
 for ks in range(min_keysize, max_keysize + 1):
-    edit_distance = hamming_distance(encrypted_file_bytes[0:ks], encrypted_file_bytes[ks:ks*2])
+    edit_distance = hamming_distance(encrypted_file_bytes[0:ks*4], encrypted_file_bytes[ks*4:ks*8])
     normal_edit_distance = float(edit_distance) / float(ks) # hopefully we don't get burned here by floating point math
     ks_edit_distance.append((normal_edit_distance, ks)) # first element of the tuple is the edit distance so we can just run sorted() on it
 sorted_ks_edit_distance = sorted(ks_edit_distance)
 sorted_ks_edit_distances = [d[1] for d in sorted_ks_edit_distance[0:3]] # "You could proceed perhaps with the smallest 2-3 KEYSIZE values"
 print("Most likely key sizes:", (", ".join([str(s) for s in sorted_ks_edit_distances])))
-# Result: Most likely key sizes: 5, 3, 2
+# Result: Most likely key sizes: 29, 5, 12
 
-"""
+best_candidate = ""
 for ks in sorted_ks_edit_distances:
     blocks = [b"" for blk in range(0, ks)]
     idx = 0
     for c in encrypted_file_bytes:
-        blocks[idx % ks] += bytes(c)
+        blocks[idx % ks] += bytes([c])
         idx += 1
     final_key = []
     for blk in blocks:
@@ -62,16 +62,9 @@ for ks in sorted_ks_edit_distances:
                 solution_key = i
         final_key.append(solution_key)
     print(f"Key at key size {ks}:", "".join([chr(c) for c in final_key]))
-"""
-# Result:
-# Most likely key sizes: 5, 3, 2
-# Key at key size 5: AAAAA
-# Key at key size 3: AAA
-# Key at key size 2: AA
+    if sorted_ks_edit_distances[0] == ks:
+        best_candidate = "".join([chr(c) for c in final_key])
 
 # Use the identified key to decrypt encrypted_file_bytes
-plaintext = repeating_key_xor(encrypted_file_bytes, b"AAAAA")
+plaintext = repeating_key_xor(encrypted_file_bytes, bytes(best_candidate, "ascii"))
 print(plaintext)
-
-# Remaining steps (probably doing this wrong):
-# For each block, the single-byte XOR key that produces the best looking histogram is the repeating-key XOR key byte for that block. Put them together and you have the key.
